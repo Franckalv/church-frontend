0576c67555614b20a3ddaba540d2ab7f6b3cb2ab
Franckalv <franciscozaalvarez@gmail.com>
2024-11-04 17:53:56 -0600

    !Adding header style and "iglesia" image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
-import MobileHeader from "./mobileHeader/MobileHeader";
+//import MobileHeader from "./mobileHeader/MobileHeader";
 
 export const routes = [
-  { name: "Home", href: "/" },
-  { name: "About", href: "/about" },
-  { name: "Contact", href: "/contact" },
-  { name: "Events", href: "/events" },
-  { name: "Ministries", href: "/ministries" },
-  { name: "Sermons", href: "/sermons" },
-  { name: "Login", href: "/login" },
+  { name: "Agenda", href: "../components/Agenda.jsx" },
+  { name: "Libros", href: "../components/Libros.jsx" },
+  { name: "Contabilidad", href: "/contact" },
+  { name: "Salones", href: "/events" },
 ];
 
 export default function Header() {
   const [open, setOpen] = useState(false);
   return (
-    <header className="w-full overflow-hidden bg-slate-300">
+    <header className="w-full overflow-hidden bg-[#BD89F1]">
       <div className="max-w-[1300px] mx-auto p-4">
-        <MobileHeader open={open} setOpen={setOpen} />
+        <button className="bg-white hover:bg-blue-700 text-black font-bold py-2 px-4 rounded-full mr-10">Agenda</button>
+        <button className="bg-white hover:bg-blue-700 text-black font-bold py-2 px-4 rounded-full mr-10">Libros</button>
+        <button className="bg-white hover:bg-blue-700 text-black font-bold py-2 px-4 rounded-full mr-10">Contabilidad</button>
+        <button className="bg-white hover:bg-blue-700 text-black font-bold py-2 px-4 rounded-full mr-10">Salones</button>
       </div>
     </header>
   );
